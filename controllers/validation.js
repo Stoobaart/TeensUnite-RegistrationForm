abfdd496b59f8e38a11ed940777a648e12b326b3
Chris Tsoi <chrisjtsoi.work@gmail.com>
2017-04-22 14:49:33 +0100

    Installed sendmail, now have the possibility to send off the email

--- controllers/validation.js
+++ controllers/validation.js
@@ -1,12 +1,16 @@
 var rpromise = require('request-promise');
+var sendmail = require('sendmail')({
+  silent: true
+});
 
 function validation(req, res) {
   console.log(req.body.postCode);
   var validated = validateAddress(req.body.postCode);
-
+validated = true;
   console.log("Validated", validated);
   if(validated) {
     // Send email
+    sendEmail(req.body);
   }
 
   res.status(200).json({
@@ -15,10 +19,6 @@ function validation(req, res) {
 }
 
 function validateDOB() {
-
-}
-
-function validateMobile() {
 
 }
 
@@ -39,12 +39,45 @@ function validateAddress(postcode) {
   rpromise(addressRequest)
         .then(function(response) {
           console.log(response);
-          if(response.statusCode !== 404 && response.statusCode!== 400) return true;
+          if(response.statusCode !== 404 && response.statusCode !== 400) return true;
           return false;
         })
         .catch(function(error) {
           return false;
         });
+}
+
+// Send the email to the relevant party
+function sendEmail(formData) {
+
+  sendmail({
+    from: formData.email,
+    to: 'chrisjtsoi.work@gmail.com',
+    subject: 'Teen Sign Up Submission',
+    html: "First Name:" + formData.firstName +
+    "<br> Last Name:" + formData.lastName +
+    "<br> Email:" + formData.email +
+    "<br> Date of Birth:" + formData.dateOfBirth +
+    "<br> Gender:" + formData.gender +
+    "<br> Address:" + formData.address +
+    "<br> Post Code:" + formData.postCode +
+    "<br> Home Telephone:" + formData.homeTel +
+    "<br> Mobile Telephone:" + formData.phoneTel +
+    "<br> Parent/Carer Name:" + formData.parentCarerName +
+    "<br> Parent/Carer Telephone:" + formData.parentCarerTel +
+    "<br> Parent/Carer Email:" + formData.parentCarerEmail +
+    "<br> Medical Condition:" + formData.medicalCondition +
+    "<br> Hospitals Attended:" + formData.hospitalAttendedsPostCode +
+    "<br> Special Requirements:" + formData.specialRequirements +
+    "<br> Social Worker:" + formData.socialWorker +
+    "<br> How Did You Head About Us:" + formData.howFind +
+    "<br> Interests:" + formData.interests +
+    "<br> Photo Release?:" + formData.photoRelease
+  }, function(error, reply) {
+    if(error) return false;
+  });
+
+  return true;
 }
 
 module.exports = {
