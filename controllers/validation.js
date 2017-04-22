34d9bb99f685fdd114969735b247d2cdd7242ade
Chris Tsoi <chrisjtsoi.work@gmail.com>
2017-04-22 13:30:00 +0100

    Added regex validation to address in backend

--- controllers/validation.js
+++ controllers/validation.js
@@ -19,7 +19,12 @@ function validateMobile() {
 
 }
 
-function validateAddress() {
+// Validates the forms address to ensure it is valid and within the UK
+function validateAddress(postcode) {
+  if(!postcode.match(/(GIR 0AA)|((([ABCDEFGHIJKLMNOPRSTUWYZ][0-9][0-9]?)|(([ABCDEFGHIJKLMNOPRSTUWYZ][ABCDEFGHKLMNOPQRSTUVWXY][0-9][0-9]?)|(([ABCDEFGHIJKLMNOPRSTUWYZ][0-9][ABCDEFGHJKSTUW])|([ABCDEFGHIJKLMNOPRSTUWYZ][ABCDEFGHKLMNOPQRSTUVWXY][0-9][ABEHMNPRVWXY])))) [0-9][ABDEFGHJLNPQRSTUWXYZ]{2})/
+)) return false;
+
+  // Set request to send to the getAddress API
   var addressRequest = {
     uri: 'https://api.getAddress.io/v2/uk/' + postcode + '?api-key=' + process.env.API_KEY_GETADDRESS ,
     json: true
@@ -27,10 +32,14 @@ function validateAddress() {
 
   rpromise(addressRequest)
         .then(function(response) {
-
+          console.log(response);
+          if(response.status !== 404 || response.status!== 400) return true;
+          return false;
         })
         .catch(function(error) {
-
+          return res.status(500).json({
+              error: error.message
+          });
         });
 }
 
